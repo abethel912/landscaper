1c82222b38aaeffb534e69b8b6035c8405241dfa
abethel912 <abethel180@gmail.com>
2023-02-04 15:24:18 -0500

    Landscaper App: user can use scissors to cut grass

--- landscaper.py
+++ landscaper.py
@@ -5,5 +5,7 @@ game = {"lawns": 0, "money": 0}
 tools = [
   {  "name": "Teeth","profit": 1,"cost": 0},
   {  "name": "Scissors","profit": 10,"cost": 5},
-  
+  {  "name": "Lawnmower","profit": 10,"cost": 25},
   ]
+
+##Game Option Function
