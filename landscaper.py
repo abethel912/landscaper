b52ed9330f63749cd1ee190b95ba55860afe3c45
abethel912 <abethel180@gmail.com>
2023-02-04 15:34:05 -0500

    landscaper: user can use push lawnmower to cut grass

--- landscaper.py
+++ landscaper.py
@@ -1,11 +1,15 @@
 # Game State
 
-game = {"lawns": 0, "money": 0}
+game = {"tool": 0, "money": 0}
 
 tools = [
   {  "name": "Teeth","profit": 1,"cost": 0},
   {  "name": "Scissors","profit": 10,"cost": 5},
-  {  "name": "Lawnmower","profit": 10,"cost": 25},
+  {  "name": "Lawnmower","profit": 25,"cost": 25},
   ]
 
 ##Game Option Function
+
+def mow_lawn():
+  tool = tools[game["tool"]]
+  print(f"")
